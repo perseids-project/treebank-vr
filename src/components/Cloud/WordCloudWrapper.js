c0eca9d8c7a3a42863449241596ed5f89dc89b4a
zfletch <zfletch2@gmail.com>
2020-08-18 11:39:22 -0400

    configure cloud formatting

--- src/components/Cloud/WordCloudWrapper.js
+++ src/components/Cloud/WordCloudWrapper.js
@@ -2,6 +2,22 @@ import React from 'react';
 import { objectOf, number } from 'prop-types';
 import ReactWordcloud from 'react-wordcloud';
 
+const options = {
+  colors: ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b'],
+  enableTooltip: true,
+  deterministic: true,
+  fontFamily: 'tinos',
+  fontSizes: [20, 80],
+  fontStyle: 'normal',
+  fontWeight: 'bold',
+  padding: 1,
+  rotations: 2,
+  rotationAngles: [-90, 0],
+  scale: 'linear',
+  spiral: 'rectangular',
+  transitionDuration: 0,
+};
+
 const convertToWordCloudFormat = (words) => {
   const wordCloudArray = [];
 
@@ -15,7 +31,7 @@ const convertToWordCloudFormat = (words) => {
 };
 
 const WordCloudWrapper = ({ words }) => (
-  <ReactWordcloud words={convertToWordCloudFormat(words)} />
+  <ReactWordcloud words={convertToWordCloudFormat(words)} options={options} />
 );
 
 export default WordCloudWrapper;
